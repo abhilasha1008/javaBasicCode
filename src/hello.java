73a3ad01196f53595a79d0e846cedbb377c68ed4
abhilasha1008 <kumariabhilasha792@gmail.com>
2023-03-02 16:18:33 +0530

    second commit

--- src/hello.java
+++ src/hello.java
@@ -1,5 +1,9 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello world");
+        int a =10;
+        int b = 20;
+        int sum = a+b;
+        System.out.println(sum);
     }
 }
