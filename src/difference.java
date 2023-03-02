d50018cede499e3e88bfd9e4bd8a32d6d686c99a
abhilasha1008 <kumariabhilasha792@gmail.com>
2023-03-02 16:22:49 +0530

    third commit

---
+++ src/difference.java
@@ -0,0 +1,8 @@
+public class difference {
+    public static void main(String[] args) {
+        int a=50;
+        int b=20;
+        int difference = a-b;
+        System.out.println(difference);
+    }
+}
